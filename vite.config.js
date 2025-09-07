256d3a7da6cc478a35086429ff5dff0a60663ed8
hugolpz <hugo.lpz@gmail.com>
2025-09-08 00:25:18 +0200

    add ghpage deployment via vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/portfolio/', // Replace 'portfolio' with your GitHub repo name
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
